6b09ea82ec866ed46343fcf6ba8a36af4315bc03
raghu545 <545uveera@gmail.com>
2025-03-19 22:34:51 +0100

    added project setup

---
+++ src/main/java/com/sap/co2_calculator/Co2CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.sap.co2_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2CalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/co2_calculator/Co2CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.co2_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
